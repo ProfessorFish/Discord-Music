4351431d1144d833559dc79288215024e85307c2
ProfessorFish <67111659+ProfessorFish@users.noreply.github.com>
2022-03-19 08:24:26 +0000

    Fix issue attempting downloading movies

--- Classes/Searcher/Functions/search.js
+++ Classes/Searcher/Functions/search.js
@@ -3,11 +3,9 @@ module.exports = async function(prompt, client){
   const ytsr = require("ytsr")
   try{
   const filters1 = await ytsr.getFilters(prompt);
-const filter1 = filters1.get('Type').get('Video');
-  const result = await ytsr(filter1.url, {
-    limit: 1
-  })
-  return result.items[0];
+  const filter1 = filters1.get('Type').get('Video');
+  const result = await ytsr(filter1.url);
+  return result.items.find(k=> k.type === "video");
   } catch(err){
     if(!await ytpl(prompt)){
       return false
